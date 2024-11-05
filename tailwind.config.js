c3d8283f8b2816f0c32c0360ac44f4dfe7b45da5
teodorSmilevski <teosmilevski@outlook.com>
2024-11-05 22:00:32 +0100

    .

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JS/TSX files in src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
